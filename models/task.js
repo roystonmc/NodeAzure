7dc564a8d3334a76da4507c9f7bf608f4ac70cef
rmcneill <rmcneill@gmail.com>
2017-08-13 16:47:51 -0400

    Simple fixes

--- models/task.js
+++ models/task.js
@@ -8,7 +8,7 @@ function Task(storageClient, tableName, partitionKey) {
    this.storageClient = storageClient;
    this.tableName = tableName;
    this.partitionKey = partitionKey;
-   this.storageClient.createContainerIfNotExists()
+
    this.storageClient.createTableIfNotExists(tableName, function tableCreated(error) {
      if(error) {
        throw error;
